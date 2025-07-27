6ac96e19da39282d9a179aab3e35b5cfa27f0f30
DanielArape17 <danielarapework@gmail.com>
2025-07-27 13:40:36 -0400

    update

--- javascript/event-driving/event-driving-programming/calculadora/main.js
+++ javascript/event-driving/event-driving-programming/calculadora/main.js
@@ -7,46 +7,79 @@
 
 // (*) Los mensajes de error se pueden mostrar con alert, pero el primero en mostrarlos en el DOM con createElement, le pone nombre al gatito por 3 días.
 
+//Variables del DOM
 const userForm = document.getElementById("calculatorForm");
 const calculateInput = document.getElementById("calculateInput");
 const btnCalculate = document.getElementById("btnCalculate");
-const btnOne = document.getElementById("btnOne")
+const btnOne = document.getElementById("btnOne");
+const btnTwo = document.getElementById("btnTwo");
+const btnThree = document.getElementById("btnThree");
+const btnFour = document.getElementById("btnFour");
+const btnFive = document.getElementById("btnFive");
+const btnSix = document.getElementById("btnSix");
+const btnSeven = document.getElementById("btnSeven");
+const btnEight = document.getElementById("btnEight");
+const btnNine = document.getElementById("btnNine");
+const btnZero = document.getElementById("btnZero");
+const btnPlus = document.getElementById("btnPlus");
+const btnMinus = document.getElementById("btnMinus");
+const btnTimes = document.getElementById("btnTimes");
+const btnDivided = document.getElementById("btnDivided");
+const btnEquals = document.getElementById("btnEquals");
 
-btnOne.addEventListener("click", () =>{
-  calculateInput.value += "1"
-})
+//Variables JS
+const symbols = ["+", "-", "*", "÷"];
+
+const btnPress = (event) =>{
+  const btn = event.target;
+  const buttonText = btn.textContent;
+  calculateInput.value += buttonText;
+  console.log(buttonText);
+}
+
+btnOne.addEventListener("click", btnPress);
+btnTwo.addEventListener("click", btnPress);
+btnThree.addEventListener("click", btnPress);
+btnFour.addEventListener("click", btnPress);
+btnFive.addEventListener("click", btnPress);
+btnSix.addEventListener("click", btnPress);
+btnSeven.addEventListener("click", btnPress);
+btnEight.addEventListener("click", btnPress);
+btnNine.addEventListener("click", btnPress);
+btnZero.addEventListener("click", btnPress);
+
+btnPlus.addEventListener("click", btnPress);
+btnMinus.addEventListener("click", btnPress);
+btnTimes.addEventListener("click", btnPress);
+btnDivided.addEventListener("click", btnPress);
+
 
 userForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  const calculate = calculateInput.value; 
-  const symbols = ["+", "-", "*", "/"];
-  
+  const calculate = calculateInput.value;
 
   symbols.forEach((symbol) => {
     if(calculate.includes(symbol)){
-      let calculateArray = calculate.split(symbol)
+      let calculateArray = calculate.split(symbol);
       let numOne = Number(calculateArray[0]);
       let numTwo = Number(calculateArray[1]);
       let result;
 
       if(symbol === "+"){
-        result = numOne + numTwo
+        result = numOne + numTwo;
       }
       else if(symbol === "-"){
-        result = numOne - numTwo
+        result = numOne - numTwo;
       }
       else if(symbol === "*"){
-        result = numOne * numTwo
+        result = numOne * numTwo;
       }
-      else if(symbol === "/"){
-        result = numOne / numTwo
-  }
-
+      else if(symbol === "÷"){
+        result = numOne / numTwo;
+      }
       const newElement = document.createElement("p");
       newElement.textContent = result;
       document.body.appendChild(newElement);
     }
   });
-
-  
 });
