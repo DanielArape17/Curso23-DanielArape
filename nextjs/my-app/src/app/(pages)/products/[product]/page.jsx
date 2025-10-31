56aa70159f3fd2b2fd8a22314450f3e31d69a2f2
DanielArape17 <danielarapework@gmail.com>
2025-10-31 17:58:49 -0400

    update

---
+++ nextjs/my-app/src/app/(pages)/products/[product]/page.jsx
@@ -0,0 +1,5 @@
+function Product( {params} ){
+  return <h1 className="text-amber-50 text-2xl">Product: {params.product}</h1> ;
+}
+
+export default Product ;
---
+++ nextjs/my-app/src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-900 text-shadow-amber-50">
+      <h1 className="text-2xl text-amber-50">Mi primera app</h1>
+    </div>
+  );
+}
